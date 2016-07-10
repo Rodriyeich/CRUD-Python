c0733e08bb496dd2599f8aa35e88675222a577b6
José Manuel Rodríguez Rodríguez <josema2r@gmail.com>
2016-07-10 23:43:32 +0200

    Añadido script inicializacion bd y captura de datos de widgets

---
+++ RegistroJuego.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Author José Manuel Rodriguez Rodriguez, Program Name: Victimas de Conan
+
+
+"""
+	Clase: RegistroConan
+	- Define un registro de la tabla: Victimas
+	- Los atributos definen los campos del registro (id, Nombre, Genero, Desarrolladora)
+"""
+class RegistroJuego:
+    #Constructor por defecto
+    def __init__(self):
+        self.__id__ = 0
+        self.__titulo__ = None
+        self.__genero__ = None
+        self.__desarrolladora__ = None
+        self.__plataforma__ = None
+        self.__lanzamiento__ = None
+        self.__caratula__ = None
+
+	#Métodos públicos
+	def getId(self):
+		return self.__id__
+
+	def setId(self,pId):
+		self.__id__ = pId
+
+    def getTitulo(self):
+		return self.__titulo__
+
+    def setTitulo(self, pTitulo):
+		self.__titulo__ = pTitulo
+
+    def getGenero(self):
+		return self.__genero__
+
+    def setGenero(self,pGenero):
+		self.__genero__ = pGenero
+
+    def getDesarrolladora(self):
+		return self.__desarrolladora__
+
+    def setDesarrolladora(self,pDesarrolladora):
+		self.__desarrolladora__ = pDesarrolladora
+
+    def setPlataforma(self,pPlataforma):
+		self.__plataforma__ = pPlataforma
+
+    def getPlataforma(self):
+		return self.__plataforma__
+
+    def setLanzamiento(self,pPlataforma):
+		self.__lanzamiento__ = pLanzamiento
+
+    def getLanzamiento(self):
+		return self.__lanzamiento__
+
+    def setCaratula(self,pCaratula):
+		self.__caratula__ = pCaratula
+
+    def getCaratula(self):
+		return self.__caratula__
--- catalogoVideojuegos.py
+++ catalogoVideojuegos.py
@@ -5,20 +5,105 @@ from gi.repository import Gtk
 import os
 import sqlite3
 
+from RegistroJuego import RegistroJuego
+
 class Catalogo_GUI:
-    builder=None
+    builder = None
+    registroJuego = None
+    entryTitulo = None
+    entryGenero = None
+    entryDesarrolladora = None
+    entryPlataforma = None
+    entryLanzamiento = None
     def __init__(self):
+        self.registroJuego = RegistroJuego()
         self.builder = Gtk.Builder()
         self.builder.add_from_file("catalogoVideojuegos.glade")
         self.handlers = {"onDeleteWindow": Gtk.main_quit,
                             "onAboutActivate": self.onAboutActivate,
                             "onCloseAbout": self.onCloseAbout,
+                            "onBotonAccionClicked": self.onBotonAccionClicked,
                             "onBotonAsignarImagenClicked": self.onBotonAsignarImagenClicked,}
 
         self.builder.connect_signals(self.handlers)
 
+        #Inicializacion widgets y valores por defecto
+        self.entryTitulo = self.builder.get_object("entryTituloDetalles")
+        self.entryGenero = self.builder.get_object("entryGeneroDetalles")
+        self.entryPlataforma = self.builder.get_object("entryPlataformaDetalles")
+        self.entryDesarrolladora = self.builder.get_object("entryDesarrolladoraDetalles")
+        self.entryLanzamiento = self.builder.get_object("entryLanzamientoDetalles")
+        self.registroJuego.setCaratula("resources/seleccionar_imagen.jpg")
+
+        #Consultamos numero de tablas que hay en la base de datos
+        conexion = sqlite3.connect("juegos.db")
+        cursor = conexion.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY Name;")
+        tablas = map(lambda t: t[0], cursor.fetchall())
+        print "Numero de tablas " + str(len(tablas))
+
+        #Si no hay tablas la creamos
+        if len(tablas) == 0:
+            cursor.execute("CREATE TABLE Juegos (id, Titulo, Genero, Desarrolladora, Plataforma, Lanzamiento, Caratula);")
+
+        #Cerramos cursor
+        cursor.close()
+        #Cerramos conexión
+        conexion.close()
+
         self.window = self.builder.get_object("ventanaPrincipal")
         self.window.show_all()
+
+    """
+    	Función que comprueba si una cadena es Nula o Vacía
+    	-Parámetros:
+    		cadena -- cadena de caractéres
+
+    	-Devuelve:
+    		True -- si detecta cadena vacía o nula
+    		False -- si la cadena no está vacía o no es nula
+    """
+    def cadenaVacia(cadena):
+
+    	if cadena is None:
+    		return True
+
+    	if cadena is "":
+    		return True
+
+    	return False
+
+    """
+    	Función que comprueba si alguno de los campos de un registro es Nulo o Vacío
+    	-Parámetros:
+    		registro -- objeto de tipo RegistroJuego
+
+    	-Devuelve:
+    		True -- si detecta alguno de los campos vacío o nulo
+    		False -- si no detecta campos vacíos o nulos
+    """
+    def camposVacios(registro):
+    	campoVacio = False
+
+    	# Comprobamos que el campo no está vacíó
+    	campoVacio = cadenaVacia(registro.getTitulo())
+
+    	if not campoVacio:
+    		campoVacio = cadenaVacia(registro.getGenero())
+
+    	if not campoVacio:
+    		campoVacio = cadenaVacia(registro.getDesarrolladora())
+
+    	if not campoVacio:
+    		campoVacio = cadenaVacia(registro.getPlataforma())
+
+    	if not campoVacio:
+    		campoVacio = cadenaVacia(registro.getLanzamiento())
+
+    	if not campoVacio:
+    		campoVacio = cadenaVacia(registro.getCaratula())
+
+    	return campoVacio
 
     def onAboutActivate(self, submenuAyudaAbout):
 		self.about = self.builder.get_object("dialogAbout")
@@ -49,8 +134,10 @@ class Catalogo_GUI:
             print("resources/" + os.path.basename(dialog.get_filename()))
             #Trunca la ruta del fichero hasta el directorio local de la app
             img.set_from_file("resources/" + os.path.basename(dialog.get_filename()))
+            self.registroJuego.setCaratula("resources/" + os.path.basename(dialog.get_filename()))
             #Asignar a bd datos
             img.show()
+            print "NOMBRE -> " + img.get_filename()
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancelar pulsado")
 
@@ -85,6 +172,38 @@ class Catalogo_GUI:
 
         dialog.destroy()
 
+    def onBotonAccionClicked(self, button):
+        conexion = sqlite3.connect("juegos.db")
+        cursor = conexion.cursor()
+
+        #Actualizamos valores de registro seleccionado
+        txt = self.entryTitulo.get_text()
+        self.registroJuego.setTitulo(txt)
+        txt = self.entryDesarrolladora.get_text()
+        self.registroJuego.setDesarrolladora(txt)
+        txt = self.entryGenero.get_text()
+        self.registroJuego.setGenero(txt)
+        txt = self.entryPlataforma.get_text()
+        self.registroJuego.setPlataforma(txt)
+        txt = self.entryLanzamiento.get_text()
+        self.registroJuego.setLanzamiento(txt)
+
+        if button.get_label is "Añadir juego":
+            if not camposVacios(registroJuego):
+            	#Consultamos los datos para extraer último id de la tabla Victimas
+            	cursor.execute("SELECT * FROM Juegos; ")
+
+            	#Extraemos longitud de la lista del cursor que coincide con el último id introducido en la tabla
+            	ultimoId = len(cursor.fetchall())
+                ulltimoId += 1
+                # Realizamos insercion de nuevo registro
+                cursor.execute("INSERT INTO Juegos(id,Titulo,Genero,Desarrolladora, Plataforma, Lanzamiento, Caratula) VALUES (%s, %s, %s, %s, %s, %s, %s);",
+                                (ultimoId, self.registroJuego.getTitulo(), self.registroJuego.getGenero(), self.registroJuego.getDesarrolladora(), self.registroJuego.getPlataforma(), self.registroJuego.getLanzamiento(), self.registroJuego.getCaratula()))
+
+            	#Cerramos cursor
+            	cursor.close()
+            	#Cerramos conexión
+            	conexion.close()
 def main():
     window = Catalogo_GUI()
     Gtk.main()
